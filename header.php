5cdc49a04d04c29a4011db05ae5d07e1148d1090
Fitz <fitzflood@gmail.com>
2014-10-19 10:43:13 -0500

    removed gogo

--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 <header>
    <div class="clearfix leading-3 fb accent white">
-        <div class="left px1 p bold leading-3"><a href="<?php echo "$baseURL"; ?>/index.php">Findborg</a></div>
+        <div class="left px1 p bold leading-3"><a href="<?php echo "$baseURL"; ?>/index.php" style="color:#000;">Findborg</a></div>
         <div class="left px1">
           <a href="#!">How To Use</a>
           <a href="typography/index.php" class="ml1">Typography</a>
